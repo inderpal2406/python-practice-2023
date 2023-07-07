bb62b5f9f507ee148a111535b02d95e1a163d481
Inderpal Singh <inderpal2406@gmail.com>
2023-07-07 20:34:45 +0530

    Enhance code in 00_ppf_calculator.py script.

--- personal-examples/00_ppf_calculator.py
+++ personal-examples/00_ppf_calculator.py
@@ -2,9 +2,22 @@
 
 # Import modules.
 
+import sys
 import custom_module_clear_screen
 
 # Define functions.
+
+def ValidateInputs(FnStartYear,FnInvestmentDuration,FnInvestmentAmount):
+    """Function to validate user provided inputs"""
+    if FnStartYear not in range(1970,2071,1):
+        print("\nEntered start year should be between 1970 to 2070. Exiting script!\n")
+        sys.exit(1)
+    elif FnInvestmentDuration not in range(15,71,5):
+        print("\nEntered tenure should be between 15 to 70 as a multiple of 5. Exiting script!\n")
+        sys.exit(1)
+    elif FnInvestmentAmount not in range(1,150000,1):
+        print("\nEntered investment amount should be between 1 to 150000. Exiting script!\n")
+        sys.exit(1)
 
 def main():
     """First function to be called"""
@@ -16,36 +29,55 @@ def main():
     """)
     input("Press ENTER key to continue.")
     while True:
-        investmentChoice = input("Will you do an annual/monthly investment? [annual|monthly]: ")
-        if investmentChoice == "annual":
-            print("Annual investments are recommended to be done in Apr every year, when financial year starts.")
-            startYear = int(input("Enter start year: "))
-            endYear = int(input("Enter end year: "))
-            investmentAmount = int(input("Enter yearly investment amount: "))
-            yearDifference = endYear - startYear
-            noOfYears = yearDifference
-            totalInvestment = investmentAmount*noOfYears
-            noOfYear = 1  # We calculate interest for each year as compounding takes place.
-            roi = 7.1
-            interest = 0
-            balAmount = 0
-            while yearDifference != 0:
-                principal = investmentAmount + balAmount
-                interest = (principal*noOfYear*roi)/100  # calculate for entire year where n=1 in (pnr)/100
-                balAmount = principal + interest
-                yearDifference = yearDifference-1
-            balAmount = int(balAmount)
-            growthInMoney = balAmount - totalInvestment
+        print()
+        print("Will you do an annual or monthly investment?")
+        print("1. Annual\n2. Monthly")
+        try:
+            InvestmentChoice = int(input("Enter choice [1|2]: "))
+        except ValueError:
+            print("\nPlease enter the numbers 1 or 2 only. Exiting script!\n")
+            sys.exit(1)
+        if InvestmentChoice == 1:
+            print("\nThis calculation will be based on investments done in Apr every year, when financial year starts.")
+            try:
+                StartYear = int(input("Enter start year [1970 to 2070]: "))
+                InvestmentDuration = int(input("Enter tenure in years [Min 15, thereafter 20,25,30...70]: "))
+                InvestmentAmount = int(input("Enter yearly investment amount [Max 150000]: "))
+            except ValueError:
+                print("\nInvalid input. Please provide integer value only for above 3 inputs. Exiting script!\n")
+                sys.exit(1)
+            ValidateInputs(StartYear,InvestmentDuration,InvestmentAmount)
+            EndYear = StartYear + InvestmentDuration
+            TotalInvestment = InvestmentAmount * InvestmentDuration
+            ROI = 7.1
+            BalAmount = 0  # Initial balance is considered as 0 when PPF account is opened.
+            Iteration = InvestmentDuration
+            while Iteration != 0:
+                Principal = InvestmentAmount + BalAmount
+                Interest = (Principal * ROI)/100  # calculate for entire year where n=1 in (pnr)/100
+                BalAmount = Principal + Interest
+                # Run loop until interest is calculated for each year for complete duration.
+                Iteration = Iteration - 1
+            BalAmount = int(BalAmount)
+            GrowthInMoney = BalAmount - TotalInvestment
+            GrowthInPercentage = round((GrowthInMoney / TotalInvestment) * 100, 2)
             print(f"""
-            An yearly investment of {investmentAmount} done every year between
-            {startYear} and {endYear}, which is for a period of {noOfYears} years,
-            would yield a maturity amount of {balAmount} rupees, on a total
-            investment of {totalInvestment} rupees, at the rate of interest (ROI) as {roi}%p.a.
-            So, total growth in money = {growthInMoney} rupees
+            Investment started in the year          : {StartYear}
+            Investment duration/tenure              : {InvestmentDuration} years
+            Investment ended in the year            : {EndYear}
+            Investment done in each year            : {InvestmentAmount}
+            Total investment done over the years    : {TotalInvestment}
+            PPF rate of interest                    : {ROI}%p.a.
+            Maturity amount                         : {BalAmount}
+            Growth happened in money                : {GrowthInMoney}
+            Growth expressed in percentage          : {GrowthInPercentage}%
+            *** Note: The maturity amount will change if ROI gets revised by govt in any quarter. ***
             """)
-            print("The maturity amount will change if ROI gets revised by govt in any quarter.")
-        elif investmentChoice == "monthly":
+        elif InvestmentChoice == 2:
             pass
+        else:
+            print("\nInvalid input. Please enter the numbers 1 or 2 only. Exiting script!\n")
+            sys.exit(1)
         userChoice = input("Do you want to repeat again? [y|n]: ")
         if userChoice == "y":
             pass
