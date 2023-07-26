28930ad0b6c7b624d5ac1669223e383f258f5249
Inderpal Singh <inderpal2406@gmail.com>
2023-07-26 15:10:25 +0530

    Update code in desktop notifier for top science news script.

--- personal-examples/top_news_desktop_notifier/script.py
+++ personal-examples/top_news_desktop_notifier/script.py
@@ -1,5 +1,11 @@
 """Module to send top news as desktop notification"""
 
+####################################################################################
+# Note: This code is tested from home laptop.
+# It may need additional changes if the machine on which this code is executed is
+# connected to VPN or behind a proxy server.
+####################################################################################
+
 # Import modules.
 
 import requests  # First install requests using pip.
@@ -15,57 +21,61 @@ import requests  # First install requests using pip.
 
 # RSS: Really Simple Syndication.
 
-RSS_FEED_URL = "http://www.hindustantimes.com/rss/topnews/rssfeed.xml"
+# RSS: RSS(Rich Site Summary, often called Really Simple Syndication) uses a family of
+# standard web feed formats to publish frequently updated informationlike blog entries,
+# news headlines, audio, video. RSS is XML formatted plain text.
 
-new_url = "https://www.hindustantimes.com/feeds/rss/top-news/rssfeed.xml"
+#RSS_FEED_URL = "http://www.hindustantimes.com/rss/topnews/rssfeed.xml"  # Old URL
+#RSS_FEED_URL = "https://www.hindustantimes.com/feeds/rss/top-news/rssfeed.xml"  # This didn't give any headline. So, we switched to science RSS feed as below.
+RSS_FEED_URL = "https://www.hindustantimes.com/feeds/rss/science/rssfeed.xml"
 
 # Define functions.
 
+def parse_XML(xml_data):
+    """Function to parse XML format data received from RSS feed"""
+
+
 def load_RSS_feed():
     """Function to load RSS feed"""
-
+    #url1 = "https://api.github.com/users/inderpal2406"
+    #url2 = "https://www.google.com"
+    # Above both URLs work without requirement for any verify and headers parameters in requests.get()
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+    #headers = {"User-Agent":"Mozilla/5.0"}  # It may work with just this much header as well.
+    #response = requests.get(RSS_FEED_URL,headers=headers)  # It works with this without need of verify argument.
+    # headers is a dict var. This var can be named anything.
+    # headers for API requests can be passed as a dictionary in request module of python.
+    # key is such a dict is the header type and value is the value for the header type.
+    # A good article on understanding headers and query parameters:
+    # https://www.torocloud.com/blog/using-query-parameters-and-headers-in-rest-api-design
+    #######################################################################################
+    # But we try to supply SSL certificates as well in the request. To supply same, we follow below procedure.
+    # Got to target website in browser, export complete chain of SSL certificates in Base64
+    # encoded pem file. Supply this file in verify argument of the request. Downloading only one
+    # certificate in the chain, in base64 encoded pem file, won't work. Complete chain is needed.
+    # When we export after viewing the cert in chrome browser, it gives option to save complete
+    # chain of certs in base64 format of .pem file. Select that option.
+    # Otherwise we need to export each certificate separately and then combine them into one 
+    # pem file as shown on website: https://levelup.gitconnected.com/solve-the-dreadful-certificate-issues-in-python-requests-module-2020d922c72f
+    cert_path = ".\\www-hindustantimes-com-chain.pem"  # This pem file can be named as anything.
+    response = requests.get(RSS_FEED_URL,headers=headers,verify=cert_path)
+    #print(response.status_code)
+    #print(response.text)
+    #print(response.content)  # content is in bytes.
+    ######################################################################################
+    # Write response.content to a file.
+    filename = ".\\topnewsfeed.xml"
+    file_object = open(filename,"wb")
+    file_object.write(response.content)
+    file_object.close()
+    return response.content
 
 def main():
     """First function to be called"""
-    load_RSS_feed()
+    rss_data = load_RSS_feed()
+    #newsitems = parse_XML(rss_data)
 
+# Call main() when the script is executed.
 
-# Got to target website in browser, export complete chain of SSL certificates in Base64
-# encoded pem file. Supply this file in verify argument of the request. Downloading only one
-# certificate in the chain, in base64 encoded pem file, won't work. Complete chain is needed.
-# When we export after viewing the cert in chrome browser, it gives option to save complete
-# chain of certs in base64 format of .pem file. Select that option.
-# Otherwise we need to export each certificate separately and then combine them into one 
-# pem file as shown on website: https://levelup.gitconnected.com/solve-the-dreadful-certificate-issues-in-python-requests-module-2020d922c72f
-
-
-url1 = "https://api.github.com/users/inderpal2406"
-url2 = "https://www.google.com"
-# Above both URLs work without requirement for any verify and headers parameters in requests.get()
-
-
-url = "https://www.hindustantimes.com/feeds/rss/top-news/rssfeed.xml"
-cert_path = "C:\\Users\\isaini\\Downloads\\server.pem"  # server.pem can be named as anything.
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
-#headers = {"User-Agent":"Mozilla/5.0"}  # It may work with just this much header as well.
-
-# headers is a dict var. This var can be named anything.
-# headers for API requests can be passed as a dictionary in request module of python.
-# key is such a dict is the header type and value is the value for the header type.
-# A good article on understanding headers and query parameters:
-# https://www.torocloud.com/blog/using-query-parameters-and-headers-in-rest-api-design
-
-
-import requests
-#response = requests.get(url)  # SSL cert related error.
-#response = requests.get(url,verify=cert_path)  # Access denied error. HTTP response as 401. 
-# Required headers as some websites don't response without headers parameters.
-response = requests.get(url,headers=headers,verify=cert_path)
-print(response.status_code)
-print(response.text)
-print(response.content)  # content is in bytes.
-
-# Above code worked initially. But later started giving below error:
-# Firewall Authentication. You must authenticate to use this service.
-# Need to check. Maybe we need to pass auth parameter in requests.get()
-
+if __name__ == "__main__":
+    main()
